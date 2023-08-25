6c20c756a6ebdd2d56817da73782191056553306
Brian Eisenberg <bheisenberg95@gmail.com>
2023-08-25 11:31:26 -0400

    update config to rename files based on project name and prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
-import { build, defineConfig } from 'vite'
+import { PluginOption, build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-
 const prefix = 'dev';
+const projectName = 'project'
+
+const renameIndexPlugin = (newFilename: string): PluginOption => {
+  if (!newFilename) return
+
+  return {
+    name: 'renameIndex',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      const indexHtml = bundle['index.html']
+      indexHtml.fileName = newFilename
+    },
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    renameIndexPlugin(`${prefix}_${projectName}.html`)
+  ],
   build: {
     sourcemap: 'inline',
     rollupOptions: {
       output: {
-        entryFileNames: `${prefix}_filename.js`,
+        entryFileNames: `${prefix}_${projectName}.js`,
         assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'index.css') {
+            return `${prefix}_${projectName}.css`
+          }
           return `${prefix}_${assetInfo.name}`
         }
       }
